88ed4395c9963fc1b5841f48bdaf434ba064d784
neisantos <tecnicoeletronei@gmail.com>
2022-03-20 10:08:43 -0400

    inclusao espacamento

--- Program.cs
+++ Program.cs
@@ -4,22 +4,26 @@
            for(int i = 1; i <= 10; i++)
            {
                Console.WriteLine($"{i} X 3 = {Multiplicar(i,3)}");
-               Console.WriteLine("*****************");
+                
+               
            }
         
         {
            for(int i = 2; i <= 10; i++)
-           {   Console.WriteLine("*****************");
-               Console.WriteLine($"{i} X 1 = {Multiplicar(i,1)}");               
-               Console.WriteLine($"{i} X 2 = {Multiplicar(i,2)}");
-               Console.WriteLine($"{i} X 3 = {Multiplicar(i,3)}");
+           {   Console.WriteLine("");
+               Console.WriteLine("*****************");
+               Console.WriteLine($"{i} X 1 = {Multiplicar(i,1)}");  
+               Console.WriteLine($"{i} X 2 = {Multiplicar(i,2)}");               
+               Console.WriteLine($"{i} X 3 = {Multiplicar(i,3)}");            
                Console.WriteLine($"{i} X 4 = {Multiplicar(i,4)}");
                Console.WriteLine($"{i} X 5 = {Multiplicar(i,5)}");
                Console.WriteLine($"{i} X 6 = {Multiplicar(i,6)}");
                Console.WriteLine($"{i} X 7 = {Multiplicar(i,7)}");
                Console.WriteLine($"{i} X 8 = {Multiplicar(i,8)}");
                Console.WriteLine($"{i} X 9 = {Multiplicar(i,9)}");
-               Console.WriteLine($"{i} X 10 = {Multiplicar(i,10)}");
+               Console.WriteLine($"{i} X 10 = {Multiplicar(i,10)}");   
+              
+               
                
            }
         }
@@ -28,6 +32,7 @@
         {
             return x * y;
         }
+        {Console.WriteLine("");}
         }
     }
     
